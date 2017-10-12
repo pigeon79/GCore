a963f8b28998d934dc5fe2cfdd37b63735e156b3
pigeon79 <pigeon79@163.com>
2017-10-12 22:38:16 +0800

    create

---
+++ G.Processor/ArgsErrorException.cs
@@ -0,0 +1,14 @@
+﻿using G.Core;
+
+namespace G.Processor
+{
+    public class ArgsErrorException : CoreException
+    {
+        public readonly int ReturnCode;
+
+        public ArgsErrorException(int returnCode, string message) : base(message)
+        {
+            this.ReturnCode = returnCode;
+        }
+    }
+}
---
+++ G.Processor/ProcessorSettings.cs
@@ -0,0 +1,37 @@
+﻿using G.Core;
+using G.Core.Setting;
+using Mono.Cecil.Cil;
+
+namespace G.Processor
+{
+    public static class ProcessorSettings
+    {
+        public static readonly string SymbolReaderProvider = "Mono.Cecil.Pdb.PdbReaderProvider, Mono.Cecil.Pdb";
+        public static readonly string SymbolWriterProvider = "Mono.Cecil.Pdb.PdbWriterProvider, Mono.Cecil.Pdb";
+        public static readonly bool AddCompareToSetProperty = true;
+
+        public static ISymbolReaderProvider GetSymbolReaderProvider()
+        {
+            return (ISymbolReaderProvider)ClassHelper.CreateInstance(SymbolReaderProvider);
+        }
+
+        public static ISymbolWriterProvider GetSymbolWriterProvider()
+        {
+            return (ISymbolWriterProvider)ClassHelper.CreateInstance(SymbolWriterProvider);
+        }
+
+        public static string GetExtName()
+        {
+            if(SymbolReaderProvider.EndsWith(".Pdb"))
+            {
+                return ".pdb";
+            }
+            return ".mdb";
+        }
+
+        static ProcessorSettings()
+        {
+            ConfigHelper.GSettings.InitClass(typeof(ProcessorSettings));
+        }
+    }
+}
